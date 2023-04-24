1c85d5c29696b116694347fe83329bf087b4bdf8
ctorrington1 <ctorrington1@gmail.com>
2023-04-24 18:48:15 +0100

    Delivery Model Endpoint

--- backend/app/app/urls.py
+++ backend/app/app/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/product/', include('product.urls')),
+    path('api/delivery/', include('delivery.urls')),
 ]
