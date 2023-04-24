75b7c5412b0cc4bd0db60bb45ba84c049f512040
everodriguezval <eve.rodriguezval@gmail.com>
2023-04-24 14:44:06 +0100

    Footer added to SharedLayout components

--- final-project/src/pages/SharedLayout.jsx
+++ final-project/src/pages/SharedLayout.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 const SharedLayout = () => {
   return (
@@ -8,6 +9,7 @@ const SharedLayout = () => {
       <section>
         <Outlet />
       </section>
+      <Footer />
     </>
   );
 };
