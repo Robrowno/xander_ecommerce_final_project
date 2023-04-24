2f967c5c2d45250d0ace43f142d5a7e64f4f8a07
ctorrington1 <ctorrington1@gmail.com>
2023-04-24 18:38:04 +0100

    Product model Serializer

--- backend/app/product/views.py
+++ backend/app/product/views.py
@@ -1,3 +1,19 @@
+from rest_framework import generics
+from .models import Product
+from .serializers import ProductSerializer
+
+# class MyModelList(generics.ListCreateAPIView):
+#     queryset = MyModel.objects.all()
+#     serializer_class = MyModelSerializer
+
+# class MyModelDetail(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = MyModel.objects.all()
+#     serializer_class = MyModelSerializer
+
+
 from django.shortcuts import render
 
 # Create your views here.
+class ProductList(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
