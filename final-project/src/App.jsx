b1a4cf1099ed99ccd09347d9a03a29f0f22fe989
everodriguezval <eve.rodriguezval@gmail.com>
2023-04-24 12:42:42 +0100

    setting up Route components for every oage

--- final-project/src/App.jsx
+++ final-project/src/App.jsx
@@ -1,34 +1,20 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <BrowserRouter>
+      <Routes >
+        <Route path="/" element={<h1>Home Page</h1>}/>
+        <Route path="products" element={<h1>Products List Page</h1>} />
+        <Route path="product" element={<h1>Single Product Page</h1>} />
+        <Route path="checkout" element={<h1>Checkout Page</h1>} />
+        <Route path="profile" element={<h1>Profile</h1>} />
+        <Route path="contact" element={<h1>Contact page</h1>} />       
+      </Routes>
+    </BrowserRouter> 
   )
 }
 
