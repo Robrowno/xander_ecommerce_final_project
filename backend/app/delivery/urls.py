b6645f0534c8ec702e184d8cf1ca52952c1fab21
ctorrington1 <ctorrington1@gmail.com>
2023-04-25 22:12:03 +0100

    Delivery Model Endpoints, Product Category all endpoint

--- backend/app/delivery/urls.py
+++ backend/app/delivery/urls.py
@@ -4,5 +4,8 @@ from django.urls import path
 from delivery import views
 
 urlpatterns = [
-    path('', views.DeliveryList.as_view(), name='delivery_list'),
+    path('', views.AllShipments.as_view(), name='shipment_list'),
+    path('<int:pk>/', views.IndividualShipment.as_view(), name='shipment_list'),
+    path('lineitems/', views.AllShipmentLineItems.as_view(), name='shipment_line_item_list'),
+    path('lineitem/<int:pk>/', views.IndividualShipmentLineItem.as_view(), name='shipment_line_item_list'),
 ]
--- backend/app/product/urls.py
+++ backend/app/product/urls.py
@@ -8,5 +8,6 @@ from product import views
 urlpatterns = [
     path('', views.AllProducts.as_view(), name='product_list'),
     path('<int:pk>/', views.IndividualProduct.as_view(), name='product_list'),
+    path('categories/', views.AllCategories.as_view(), name='product_list'),
     path('category/<int:pk>/', views.CategoryProducts.as_view(), name='product_list'),
 ]
