576d83d3180f9ee979cc144b6decc419b3c67d98
mjoseph26 <mathew.joseph@xandertalent.com>
2023-04-26 07:00:23 +0100

    Added layout styling for product filter page

--- final-project/src/components/ProductItem.jsx
+++ final-project/src/components/ProductItem.jsx
@@ -1,12 +1,12 @@
 const ProductItem = (props) => {
     const {key,img,alt,name,price,description,rating} = props;
     return (
-        <article key={key}>
+        <article className="product-display" key={key}>
             <img src={img} alt={alt}/>
             <h4>{name}</h4>
-            <h5>{price}</h5>
-            <p>{description}</p>
-            <p>{rating}</p>
+            <h5>${price}</h5>
+            <p>Product Rating: {rating}/5</p>
+            <button className="view-details" type="button">View Details</button>
         </article>
     )
 } 
